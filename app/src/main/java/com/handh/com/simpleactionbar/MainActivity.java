d4a07a10072e5306895e127bbdabdc0c727948e1
fallag113 <An0nym0usgass@gmail.com>
2017-06-23 05:55:23 +0200

    new app bar

--- app/src/main/java/com/handh/com/simpleactionbar/MainActivity.java
+++ app/src/main/java/com/handh/com/simpleactionbar/MainActivity.java
@@ -1,13 +1,28 @@
 package com.handh.com.simpleactionbar;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.toolbar=(Toolbar) findViewById(R.id.action_bar);
+        this.toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(this.toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu,menu);
+        return true;
     }
 }
